34f758293e6f62792769875714e4a049388088cf
Mohammad Khodadad <khodadadmohammad1997@gmail.com>
2025-01-07 14:44:16 -0500

    added o1

--- evaluate_pipeline.py
+++ evaluate_pipeline.py
@@ -5,7 +5,7 @@ import multiprocessing
 from tqdm import tqdm
 from utils.pipeline import Pipeline
 from data_loader.utils.answer_evaluation import bulk_evaluation
-from utils.llm import gpt4o_query
+from utils.llm import gpt_query
 
 # Global variables for the pipeline and API key
 pipeline = None
@@ -30,9 +30,10 @@ def process_query(args):
     our_response = pipeline.process_query(
         question, top_k=50, max_iterations=3, iterative_retrival_k=3, hybrid=True
     )
-    gpt_response = gpt4o_query(question, api_key)
+    gpt4o_response = gpt_query(question, api_key)
+    gpto1_response = gpt_query(question, api_key, 'o1')
 
-    return index, answer, our_response, gpt_response
+    return index, answer, our_response, gpt4o_response, gpto1_response
 
 def main():
     dotenv.load_dotenv()
@@ -43,13 +44,15 @@ def main():
 
     references = []
     our_answers = []
-    gpt_answers = []
+    gpt4o_answers = []
+    gpto1_answers = []
+
 
     # Limit the number of processes to prevent resource exhaustion
     num_processes = min(4, multiprocessing.cpu_count())
 
     # Prepare arguments for each query
-    args = [(i, qas[i]) for i in range(10)]  # Adjust range as needed
+    args = [(i, qas[i]) for i in range(len(qas))]  # Adjust range as needed
 
     # Initialize the pool with the initializer function
     with multiprocessing.Pool(processes=num_processes, initializer=init_processes) as pool:
@@ -59,16 +62,17 @@ def main():
     results.sort()
 
     # Collect the results
-    for _, answer, our_response, gpt_response in results:
+    for _, answer, our_response, gpt4o_response, gpto1_response in results:
         references.append(answer)
         our_answers.append(our_response)
-        gpt_answers.append(gpt_response)
+        gpt4o_answers.append(gpt4o_response)
+        gpto1_answers.append(gpto1_response)
 
     # Evaluate the answers
-    print("Ours:")
-    print(bulk_evaluation(references, our_answers,os.environ.get("OPENAI_API_KEY")))
-    print("GPT's:")
-    print(bulk_evaluation(references, gpt_answers,os.environ.get("OPENAI_API_KEY")))
-
+    answers={}
+    answers['ours'] = bulk_evaluation(references, our_answers,os.environ.get("OPENAI_API_KEY"))
+    answers['gpt4o'] = bulk_evaluation(references, gpt4o_answers,os.environ.get("OPENAI_API_KEY"))
+    answers['gpto1'] = bulk_evaluation(references, gpto1_response,os.environ.get("OPENAI_API_KEY"))
+    print(answers)
 if __name__ == '__main__':
     main()
--- utils/llm.py
+++ utils/llm.py
@@ -3,7 +3,7 @@ import openai
 from openai import OpenAI
 # Once there was a boy, who saw his father smiling at him just before he passed away. He always sorrowed about that moment, until he saw his son standing by his deathbed.
 # He just then realized, His father was also thinking about his own father. That was when he too smiled. 
-def gpt4o_query(question,api_key):
+def gpt_query(question,api_key,model='gpt-4o'):
     client = OpenAI(api_key=api_key)
     response = client.chat.completions.create(
                 messages=[
