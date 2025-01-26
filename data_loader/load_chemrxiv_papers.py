fea0d5afc8a83f696e33b4992e59430bedb9c620
Mohammad Khodadad <khodadadmohammad1997@gmail.com>
2025-01-26 12:43:32 -0500

    wrote the part to download papers and clean them and store them

--- data_loader/load_chemrxiv_papers.py
+++ data_loader/load_chemrxiv_papers.py
@@ -2,8 +2,8 @@ import json
 from utils.chemrxiv import chemrxiv_download_papers, chemrxiv_fetch_all_papers_from_2024, process_pdfs_to_dataframe
 
 
-# all_papers=chemrxiv_fetch_all_papers_from_2024(output_file="./data/chemrxiv_data_2024.json",total=50)
-# with open('./data/chemrxiv_data_2024.json','rb') as f:
-#     all_papers=json.load(f)
-# chemrxiv_download_papers(all_papers,'./data/chemrxiv_papers')
+all_papers=chemrxiv_fetch_all_papers_from_2024(output_file="./data/chemrxiv_data_2306.json",total=15000)
+with open('./data/chemrxiv_data_2306.json','rb') as f:
+    all_papers=json.load(f)
+chemrxiv_download_papers(all_papers,'./data/chemrxiv_papers')
 process_pdfs_to_dataframe('./data/chemrxiv_papers','./data/chemrxiv_data.csv')
