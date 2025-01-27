5656b27b2c93161a62250646341428716fe6b7dc
Mohammad Khodadad <khodadadmohammad1997@gmail.com>
2025-01-27 01:12:45 -0500

    fixed a bug with ner and added it to keyword extractor

--- utils/retriever.py
+++ utils/retriever.py
@@ -26,7 +26,7 @@ class EmbeddingFunc(EmbeddingFunction):
 
 class Retriever:
     def __init__(self, model_name: str = "all-mpnet-base-v2", chroma_dir: str = "chroma_db",
-                  whoosh_dir: str = "whoosh_index", ner_model_name: str ="dbmdz/bert-large-cased-finetuned-conll03-english",
+                  whoosh_dir: str = "whoosh_index", ner_model_name: str ="pruas/BENT-PubMedBERT-NER-Chemical",
                  cross_encoder_model_name: str = "cross-encoder/ms-marco-MiniLM-L-12-v2"):
         """
         Initialize the retriever class.
